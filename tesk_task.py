a0e92de146c62bc79f1c06159cd8a68c56997779
konstantin_solovev <kostyslv@yandex.ru>
2023-05-23 21:41:03 +0700

    [upd] better

---
+++ tesk_task.py
@@ -0,0 +1,23 @@
+import datetime
+
+class Book:
+
+    def __init__(self, title: str, author: str, year: int):
+        self.title = title
+        self.author = author
+        self.year = year
+
+    def get_info(self):
+        return f'Название книги: {self.title}, ' \
+               f'автор книги: {self.author}, ' \
+               f'год издания: {self.year}'
+
+    def get_year(self):
+        current_year = datetime.date.today().year
+        return f'Возраст книги: {current_year - self.year} лет'
+
+book = Book('Отцы и дети', 'Тургенев', 1862)
+info = book.get_info()
+age = book.get_year()
+print(info)
+print(age)
